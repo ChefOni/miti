6e5667a9938d7c0d605a10ea4f85890bfce058a5
OniTomiwa <tomiwa.o@japa.run>
2026-01-24 16:19:31 +0100

    feat:Set up rn basics

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,7 +2,13 @@
 import { Stack } from "expo-router";
 import "../global.css"
 
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 
+const queryClient= new QueryClient()
 export default function RootLayout() {
-  return <Stack />;
+  return <QueryClientProvider client={queryClient}>
+    <Stack screenOptions={{
+      headerShown:false ,
+    }} />
+  </QueryClientProvider>;
 }
